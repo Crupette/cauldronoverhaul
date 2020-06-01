9e5b40d43f157edb7cba600577e9a5e2771ab8b3
Jon Santmyer <jonsantmyer@gmail.com>
2020-06-01 11:38:18 -0400

    fix instant potions deleting cauldron

--- src/main/java/me/crupette/cauldronoverhaul/fluidpotions/FluidPotionsModIntegration.java
+++ src/main/java/me/crupette/cauldronoverhaul/fluidpotions/FluidPotionsModIntegration.java
@@ -2,6 +2,7 @@ package me.crupette.cauldronoverhaul.fluidpotions;
 
 import me.crupette.cauldronoverhaul.actions.CauldronActions;
 import me.crupette.cauldronoverhaul.util.BucketActionTransformer;
+import me.crupette.cauldronoverhaul.util.CauldronBlockTransformer;
 
 public class FluidPotionsModIntegration {
 
@@ -9,5 +10,6 @@ public class FluidPotionsModIntegration {
         CauldronActions.addAction(new CauldronActionPotion());
 
         BucketActionTransformer.addTransformer(new PotionBucketActionTransformer());
+        CauldronBlockTransformer.addTransformer(new PotionCauldronBlockTransformer());
     }
 }
